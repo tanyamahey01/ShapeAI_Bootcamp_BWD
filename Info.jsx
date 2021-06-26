97e1716d9f226557b56aad10f72dced00a09aaff
tanyamahey01 <86479656+tanyamahey01@users.noreply.github.com>
2021-06-26 21:19:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div class="note">
+      <h1>JavaScript & React.js </h1>
+      <p>A Basic web dev React js Bootcamp </p>
+    </div>
+  );
+}
+export default Info;
